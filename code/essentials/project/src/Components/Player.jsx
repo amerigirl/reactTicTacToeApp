23f1c4e855a455d87e0e0227541a37ce7a90b865
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-01-04 17:25:00 -0800

    updating editing functionality

--- code/essentials/project/src/Components/Player.jsx
+++ code/essentials/project/src/Components/Player.jsx
@@ -3,19 +3,23 @@ import { useState } from "react";
 /**
  * Player component
  */
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
 
   function handleEditClick() {
-    setIsEditing((editing) => !editing); //add the function call
+    setIsEditing((editing) => !editing); //add the function call to deal with possible schedule issues
+
   }
 
-  let playerName = <span className="player-name">{name}</span>;
+
+//   let editiblePlayerName = <span className="player-name">{playerName}</span>;
 
   return (
     <li>
       <span className="player">
-        {isEditing ? <input type="text" required value={name} /> : playerName}
+        {isEditing ? <input type="text" required /> : playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
