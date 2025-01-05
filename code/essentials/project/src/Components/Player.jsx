11d60fe0b4eeda0e13109d6b4b542330d04eb705
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-01-04 16:31:29 -0800

    creating is editing functionality

--- code/essentials/project/src/Components/Player.jsx
+++ code/essentials/project/src/Components/Player.jsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
+
 /**
  * Player component
  */
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {isEditing ? <input type="text" required /> : playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>edit</button>
     </li>
   );
 }
