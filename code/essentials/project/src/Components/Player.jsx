4978570619daa56651765088dd5428aef5d27353
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-01-04 16:52:17 -0800

    update toggle function

--- code/essentials/project/src/Components/Player.jsx
+++ code/essentials/project/src/Components/Player.jsx
@@ -7,7 +7,7 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing((editing) => !editing); //add the function call
   }
 
   let playerName = <span className="player-name">{name}</span>;
@@ -15,10 +15,10 @@ export default function Player({ name, symbol }) {
   return (
     <li>
       <span className="player">
-        {isEditing ? <input type="text" required  /> : playerName}
+        {isEditing ? <input type="text" required value={name} /> : playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>{isEditing ? "save" : "edit"}</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
