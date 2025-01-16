f1f2b6093cebd7446def1a890aa3b7ce9ca165f0
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-01-16 13:00:56 -0800

    updated logging

--- code/essentials/project/src/Components/Log.jsx
+++ code/essentials/project/src/Components/Log.jsx
@@ -1,7 +1,6 @@
-export default function Log(){
+export default function Log({turns}){
    
    
    return <ol id="log">
-        THIS IS THE LOG COMPONENT
-    </ol>
+{turns.map(turn => <li key={`${turn.square.row}${turn.square.col}`}>{turn.player} selected {turn.square.row}, {turn.square.col}</li>)}    </ol>
 }
